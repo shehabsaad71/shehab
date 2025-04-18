492bd0a3c149e3d73e6a8c483406bbbde0f56c12
shehabsaad71 <fuuytf1@gmail.com>
2025-04-18 13:16:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-const darkToggle = document.querySelector('.toggle-dark');
-const body = document.body;
 
-darkToggle.addEventListener('click', () => {
-    body.classList.toggle('light-mode');
-    body.classList.toggle('dark-mode');
-
-    const icon = darkToggle.querySelector('span');
-    icon.textContent = icon.textContent === 'wb_sunny' ? 'dark_mode' : 'wb_sunny';
-});
 
 // Typing effect logic
 const typingElement = document.querySelector('.typing');
